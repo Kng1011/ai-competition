1321f96db34f4781f5ee4b3b2ab15d78e63e8afc
Joel Filipe Martins Amaral <131992612+Kng1011@users.noreply.github.com>
2024-04-16 10:20:38 +0100

    Add files via upload

---
+++ src/games/minesweeper/simulator.py
@@ -0,0 +1,42 @@
+from src.games.minesweeper.action import MinesweeperAction
+from src.games.minesweeper.player import MinesweeperPlayer
+from src.games.minesweeper.state import MinesweeperState
+from src.games.game_simulator import GameSimulator
+
+
+class MinesweeperSimulator(GameSimulator):
+
+    def __init__(self, players, num_rows: int = 7, num_cols: int = 7):
+        super(MinesweeperSimulator, self).__init__(players)
+        """
+        the number of rows and cols from the Minesweeper grid
+        """
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+
+    def on_init_game(self):
+        return MinesweeperState(self.__num_rows, self.__num_cols)
+
+    def on_before_end_game(self, state: MinesweeperState):
+        # ignored for this simulator
+        pass
+
+    def on_end_game(self, state: MinesweeperState):
+        # ignored for this simulator
+        pass
+
+    def on_state_update(self, state):
+        # ignored for this simulator
+        pass
+
+    @staticmethod
+    def get_player_type():
+        return MinesweeperPlayer
+
+    @staticmethod
+    def get_state_type():
+        return MinesweeperState
+
+    @staticmethod
+    def get_action_type():
+        return MinesweeperAction
